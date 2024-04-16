6074f69cbcd6b2f98d5529283dcc1dfbee4027f8
ayushijain3 <96405834+ayushijain3@users.noreply.github.com>
2024-04-16 11:06:47 +0530

    plpaywrightnew

--- login.js
+++
@@ -1,7 +0,0 @@
-const loginInfo = {
-  username: "kailash.pathak@3pillarglobal.com",
-  password: "Test@12345",
-  loginUrl: "https://safetyhub-stg.playershealth.com/",
-};
-
-module.exports = loginInfo;
