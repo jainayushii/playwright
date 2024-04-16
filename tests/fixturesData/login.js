94e0d3e67bf10f7bf1c50200d01456a2388a2fed
ayushijain3 <96405834+ayushijain3@users.noreply.github.com>
2024-04-16 11:00:45 +0530

    plpaywrightnew

---
+++ tests/fixturesData/login.js
@@ -0,0 +1,7 @@
+const loginInfo = {
+  username: "kailash.pathak@3pillarglobal.com",
+  password: "Test@12345",
+  loginUrl: "https://safetyhub-stg.playershealth.com/",
+};
+
+module.exports = loginInfo;
